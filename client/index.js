3f51afb5f65eb68c3cadd328a517703ef97a2ab5
Ionică Bizău <bizauionica@yahoo.com>
2015-04-09 13:01:45 +0300

    sel instead of selector

--- client/index.js
+++ client/index.js
@@ -18,5 +18,5 @@ var $ = require("/libs/jquery");
  */
 exports.jq = function (ev, data) {
     data.args = data.args || [];
-    $.fn[data.meth].apply($(data.selector), data.args);
+    $.fn[data.meth].apply($(data.sel), data.args);
 };
